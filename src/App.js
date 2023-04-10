a930619ae428bfdb31601ab2092ad39b49b125c4
CheHyeonyeong <gusdudco6@naver.com>
2023-04-10 16:07:33 +0900

    UI @material-ui/core로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo'
 import './App.css';
+import {Paper, List} from "@material-ui/core"
 
 class App extends React.Component {
 
@@ -16,16 +17,20 @@ class App extends React.Component {
     };  
   }
   render(){
-    var todoItems = this.state.item.map((item, idx)=>(
-      <Todo item={item} key={item.id} />
-    ))
+    var todoItems = this.state.item.length >0 && (
+      <Paper style={{margin:16}}>
+        <List>
+          {this.state.item.map((item, idx)=> (
+            <Todo item={item} key={item.id}/>
+          ))}
+        </List>
+      </Paper>
+    );
     //나는 item으로 하고 싶어서 this.state에 item만 넣어둠.
     //map = 원소를 배열로 하나씩 바꿔줌
     return (
       <div className='App'>
-        {
-        /* todo 컴포넌트를 여러개로 만들어서 배열로 내보낼 수 있도록함 */
-        }
+        
         {todoItems}
       </div>
     );   
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core"
+
 class Todo extends React.Component{
     constructor(props){
         super(props);
@@ -8,16 +10,22 @@ class Todo extends React.Component{
         };
     }
     render(){
+        const item= this.state.item;
         return(
-            <div className="Todo">
-                <input 
-                type="checkbox"
-                id={this.state.item.id}
-                name={this.state.item.id}
-                checked={this.state.item.done}
-                />
-                <label id = {this.state.item.id}>{this.state.item.title}</label>
-            </div>
+            <ListItem>
+                <Checkbox checked={item.done} />
+                <ListItemText>
+                    <InputBase 
+                        inputProps={{"aria-label": "naked"}}
+                        type="text"
+                        id={item.id}
+                        name={item.id}
+                        value={item.title}
+                        multiline = {true}
+                        fullWidth = {true}
+                    />
+                </ListItemText>
+            </ListItem>
         )
     }
 }
