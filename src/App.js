c238fda7d44b4de0f6a88da2bdb970d00f6ee4e6
CheHyeonyeong <gusdudco6@naver.com>
2023-04-12 13:15:43 +0900

    event handler를 통해 todo 생성 UI 짜기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Todo from './Todo'
+import Todo from './Todo';
+import AddTodo from './AddTodo.js';
 import './App.css';
-import {Paper, List} from "@material-ui/core"
+import {Paper, List, Container} from "@material-ui/core";
 
-class App extends React.Component {
+
+class App extends React.Component { 
 
   constructor(props){
     super(props);
@@ -13,9 +15,22 @@ class App extends React.Component {
           {id:1, title:"hello World2", done: false} ,
           {id:3, title:"소스코드를 고치면 알아서 해줍니다", done: false} 
           //값을 배열로 받았으므로 배열로 내보낼 수 있게끔 코드를 짜야 함
+          //배열에 추가하는 함수를 자식에 보내는 방법 or 값을 모두 배열로 만들어서 prop를 통해 자식에게 보낼 수 있음
         ]
     };  
   }
+
+  //추가하고자 하는 item이 오면 setting 과정을 지나서 Additem에 아이템을 넣어준다.
+
+  add = (item) => {
+    const thisItems = this.state.item;
+    item.id="ID-" + thisItems.length//key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items : ", this.state.item);
+  }
+
   render(){
     var todoItems = this.state.item.length >0 && (
       <Paper style={{margin:16}}>
@@ -30,8 +45,10 @@ class App extends React.Component {
     //map = 원소를 배열로 하나씩 바꿔줌
     return (
       <div className='App'>
-        
-        {todoItems}
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/> 
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
       </div>
     );   
   }
